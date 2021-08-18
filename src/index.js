6b9df4462db67c1ed81c20c900c33291a10c6696
Daniel Samuel <dansam5000@gmail.com>
2021-08-18 18:03:16 +0100

    Modify html layout & add more styles

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import _ from 'lodash';
 import './style.css';
 
-// function component() {
-//     const element = document.createElement('h1');
-  
-//     // Lodash, now imported by this script
-//     element.innerHTML = _.join(['To', 'Do', 'List'], ' ');
-//     element.classList.add('header');
-  
-//     return element;
-//   }
-  
-//   document.body.appendChild(component());
+
+// const elements = []
+// let addElement = () => {
+//    if(document.querySelector(".add-txt").value.trim() != ""){
+//        elements.push(document.querySelector(".add-txt").value.trim());
+//        alert(elements);
+//    }
+// }
