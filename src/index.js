c2951f836aaf127f8b7c576b18ef36473b790d10
Daniel Samuel <dansam5000@gmail.com>
2021-08-19 09:30:22 +0100

    Add completed class style and Listen to event

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
 import './style.css';
-import { addTodo } from './todo.js';
+import { addTodo, deleteCheck } from './todo.js';
 
 
-//const todoInput = document.querySelector('.todo-input');
+// const todoInput = document.querySelector('.todo-input');
+const todoList = document.querySelector('.todo-list');
 const todoButton = document.querySelector('.todo-button');
 
 // Event Listeners
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
