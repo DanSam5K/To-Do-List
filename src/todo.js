18470d6e287bd445fedd219388bcf5b4abba07fd
Daniel Samuel <dansam5000@gmail.com>
2021-08-19 12:23:02 +0100

    Add filter option function

--- src/todo.js
+++ src/todo.js
@@ -5,7 +5,8 @@ const todoInput = document.querySelector('.todo-input');
 //const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 
-const addTodo = (event) => {
+
+const addTodo = event => {
     // Prevent form from submitting
     event.preventDefault();
     // Todo div
@@ -39,15 +40,19 @@ const addTodo = (event) => {
     //Clear input value
     todoInput.value = '';
   };
-
-  const deleteCheck = (event) => {
+  
+  const deleteCheck = event => {
      const item = event.target;
-     //Delete Todo
-     if(item.classList[0] === "trash-btn"){
-        const todo = item.parentElement;
-        todo.classList.add('fall')
-        todo.remove();
-     }
+    // Delete Todo
+    //  if(item.classList[0] === "trash-btn"){
+    //     const todo = item.parentElement;
+    //     //Animation
+    //     todo.classList.add('fall');
+    //     todo.addEventListener('transitionend', () => {
+    //         todo.remove();
+    //     })
+        
+    //  }
     // Check mark
      if(item.classList[0] === "complete-btn"){
         const todo = item.parentElement;
@@ -55,4 +60,29 @@ const addTodo = (event) => {
      }
   }
 
-  export { addTodo, deleteCheck };
+  const filterTodo = event => {
+      const todo = todoList.childNodes();
+      todoList.forEach(todo => {
+          switch(event.target.value){
+            case "all":
+                todo.style.display = "flex"
+              break;
+            case "completed":
+                if(todo.classList.contains('completed')){
+                    todo.style.display = "flex"
+                } else {
+                    todo.style.display = "none";
+                }
+              break;
+            case "uncompleted":
+                if(!todo.classList.contains('completed')){
+                    todo.style.display = "flex"
+                } else {
+                    todo.style.display = "none";
+                }
+              break;
+          }
+      })
+  }
+
+  export { addTodo, deleteCheck, filterTodo };
