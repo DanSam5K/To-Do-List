54fa8f348ea3fac088a4bbe3f734e469d9ebea79
Daniel Samuel <dansam5000@gmail.com>
2021-08-19 12:21:20 +0100

    Add event to listen to filter option

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import _ from 'lodash';
 import './style.css';
-import { addTodo, deleteCheck } from './todo.js';
+import { addTodo, deleteCheck, filterTodo} from './todo.js';
 
 
 // const todoInput = document.querySelector('.todo-input');
 const todoList = document.querySelector('.todo-list');
 const todoButton = document.querySelector('.todo-button');
+const filterOption = document.querySelector('.filter-todo');
 
 // Event Listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
