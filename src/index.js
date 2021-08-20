272567e1819749066f422176ade5e46ac8eaf246
Daniel Samuel <dansam5000@gmail.com>
2021-08-20 12:07:12 +0100

    Pass instantiated class pass unto UI

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import _ from 'lodash';
 import './style.css';
 import filterTodo from './todo.js';
-import Todo from './uiclass.js';
+import Todo from './todoclass.js';
 import UI from './uiclass.js';
 
 const todoList = document.querySelector('.todo-list');
 const todoButton = document.querySelector('.todo-button');
 const filterOption = document.querySelector('.filter-todo');
-const todoForm = document.querySelector("#todo-form")
-
-// class constructor
-// class Todo {
-//   constructor(completed, description, index) {
-//     this.completed = completed;
-//     this.description = description;
-//     this.index = index;
-//   }
-// }
+const todoForm = document.querySelector("#todo-form");
 
 // Event Listeners
 document.addEventListener('DOMContentLoaded', UI.displayTodo);
@@ -25,13 +16,17 @@ document.addEventListener('DOMContentLoaded', UI.displayTodo);
 todoForm.addEventListener('submit', (event) => {
    //prevent default submit
    event.preventDefault();
-  const todoValue = document.querySelector("#description").value;
+  const completed = '<i class="fas fa-check"></i>';
+  const description = document.querySelector("#description").value;
+  const edit = '<i class="fas fa-edit"></i>';
+  const index = '<i class="fas fa-trash"></i>';
+
    //instantiate a todo
-   const todo = new Todo(description)
+   const todo = new Todo(completed, description, edit, index)
    console.log(todo);
 
    //Add book to list from the UI
-   
+   UI.addTodoToList(todo);
 })
 
 todoList.addEventListener('click', UI.deleteCheck);
