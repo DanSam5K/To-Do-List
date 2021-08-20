a1ea5e9efd6e90f34664608b5f3f44a9cd3c9daa
Daniel Samuel <dansam5000@gmail.com>
2021-08-20 09:53:57 +0100

    Instantiate a book object from user input

--- src/index.js
+++ src/index.js
@@ -1,22 +1,38 @@
 import _ from 'lodash';
 import './style.css';
 import filterTodo from './todo.js';
+import Todo from './uiclass.js';
 import UI from './uiclass.js';
 
 const todoList = document.querySelector('.todo-list');
 const todoButton = document.querySelector('.todo-button');
 const filterOption = document.querySelector('.filter-todo');
+const todoForm = document.querySelector("#todo-form")
 
 // class constructor
-class Todo {
-  constructor(completed, description, index) {
-    this.completed = completed;
-    this.description = description;
-    this.index = index;
-  }
-}
+// class Todo {
+//   constructor(completed, description, index) {
+//     this.completed = completed;
+//     this.description = description;
+//     this.index = index;
+//   }
+// }
 
 // Event Listeners
 document.addEventListener('DOMContentLoaded', UI.displayTodo);
+
+//Add Book Event
+todoForm.addEventListener('submit', (event) => {
+   //prevent default submit
+   event.preventDefault();
+  const todoValue = document.querySelector("#description").value;
+   //instantiate a todo
+   const todo = new Todo(description)
+   console.log(todo);
+
+   //Add book to list from the UI
+   
+})
+
 todoList.addEventListener('click', UI.deleteCheck);
 filterOption.addEventListener('click', filterTodo);
